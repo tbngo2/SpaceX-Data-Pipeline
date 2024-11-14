5cd9a861e61d57cc060bb58aeeb3cef77f2cee1d
Tram Ngo <tramngo@Trams-Air.Home>
2024-11-13 18:02:19 -0700

    variables.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello, SpaceX Data Engineer!")
+
+launch_count = 10
+
+success_rate = 0.95
+
+rocket_name =  "Falcon 9"
+
+is_active = True
+
+total_launches = launch_count + 5
+print(total_launches)
---
+++ variables.py
@@ -0,0 +1,7 @@
+total_launches = 0
+rocket_name = "Falcon 9"
+success_rate = 0.95
+
+print(total_launches)
+print(rocket_name)
+print(success_rate)
